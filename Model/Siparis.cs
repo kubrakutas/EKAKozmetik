99e96e6a193714587ba80a1bf77265beb1231215
kubrakutas <49562569+kubrakutas@users.noreply.github.com>
2019-05-28 18:09:52 +0300

    Add files via upload

---
+++ Model/Siparis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKAKozmetik.Model
+{
+    public class Siparis
+    {
+        public int ID { get; set; }
+        public string UrunIsim { get; set; }
+        public double ToplamFiyat { get; set; }
+        public int Adet { get; set; }
+        public DateTime Tarih { get; set; }
+        public string KargoDurumu { get; set; }
+    }
+}
